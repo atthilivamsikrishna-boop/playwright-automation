da6d2daf075e72ed20fefce176724917bfc5a5b8
vamsi891-git <vamsi.atthili@bestinfra.tech>
2026-03-06 12:21:48 +0530

    Fix CI pipeline headless browser issue

--- playwright.config.ts
+++ playwright.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
   ],
   use: {
     baseURL: process.env.BASE_URL,
-    headless: false,
+    headless: process.env.CI ? true : false,
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
     trace: 'retain-on-failure',
